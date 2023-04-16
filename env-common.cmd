7eb398101eb426270be1405710fb722544abe83b
Jarkko Koivikko <jarkko.koivikko@code-q.fi>
2023-04-16 17:05:18 +0300

    Allow to set QT_ROOT from outside of build

--- env-common.cmd
+++ env-common.cmd
@@ -1,7 +1,9 @@
 @echo off
 
-set QT_ROOT=C:\Qt\6.3.1
-if exist env-custom.cmd call env-custom.cmd
+if "%QT_ROOT%"=="" (
+    set QT_ROOT=C:\Qt\6.3.1
+    if exist env-custom.cmd call env-custom.cmd
+)
 
 set QT_MSVC=%QT_ROOT%\msvc2019_64
 set QT_MINGW=%QT_ROOT%\mingw_64
